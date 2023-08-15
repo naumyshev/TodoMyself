76a2d443e313241def3699bf75570ea41e27004e
Aleksei Naumyshev <naumyshev@yandex.ru>
2023-08-15 07:46:08 +0300

    lesson 1 completed

---
+++ src/Todolist.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+export type TodolistPropsType = {
+    title: string
+    tasks: Array<TaskType>
+}
+
+export type TaskType = {
+    id: number
+    title: string
+    isDone: boolean
+}
+
+export const Todolist = (props: TodolistPropsType) => {
+    return (
+        <div>
+            <h3>{props.title}</h3>
+            <div>
+                <input/>
+                <button>+</button>
+            </div>
+            <ul>
+                {props.tasks.map(el => {
+                    return (
+                        <li><input type="checkbox" checked={el.isDone}/> <span>{el.title}</span></li>
+                    )
+                })}
+            </ul>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
+        </div>
+    );
+};
+
