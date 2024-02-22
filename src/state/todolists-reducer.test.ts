3fee932bbdbb477c7c8e20fe7fde86ad2fc1f2f9
Aleksei Naumyshev <naumyshev@yandex.ru>
2024-02-22 21:43:54 +0300

    lesson 8: added test 'correct todolist should be added'

--- src/state/todolists-reducer.test.ts
+++ src/state/todolists-reducer.test.ts
@@ -17,3 +17,20 @@ test('correct todolist should be removed', ()=>{
     expect(endState.length).toBe(1)
     expect(endState[0].id).toBe(todolistId2)
 })
+
+test('correct todolist should be added', ()=>{
+    const todolistId1 = v1()
+    const todolistId2 = v1()
+
+    const newTodolistTitle = 'New Todolist'
+
+    const startState: Array<TodolistType> = [
+        {id: todolistId1, title: 'What to learn', filter: 'All'},
+        {id: todolistId2, title: 'What to buy', filter: 'All'}
+    ]
+
+    const endState = todolistsReducer(startState, {type: 'ADD-TODOLIST', title: newTodolistTitle})
+
+    expect(endState.length).toBe(3)
+    expect(endState[2].title).toBe(newTodolistTitle)
+})
