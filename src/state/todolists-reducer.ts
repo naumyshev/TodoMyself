6f895a146974784fb4a1688911f28e9fa186a708
Aleksei Naumyshev <naumyshev@yandex.ru>
2024-03-17 20:18:16 +0300

    lesson 8: TDD added case changeTodolistFilter

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -1,4 +1,3 @@
-
 import {TodolistType} from "../App";
 import {v1} from "uuid";
 
@@ -10,15 +9,15 @@ type ActionType = {
 export const todolistsReducer = (state: Array<TodolistType>, action: ActionType) => {
     switch (action.type) {
         case 'REMOVE-TODOLIST':
-            return state.filter(td=>td.id !== action.id)
+            return state.filter(td => td.id !== action.id)
         case 'ADD-TODOLIST':
             const newTodolistId = v1()
             const newTodolist: TodolistType = {id: newTodolistId, title: action.title, filter: 'All'}
             return [newTodolist, ...state]
         case 'CHANGE-TODOLIST-TITLE':
-
-            return (state.map(tl=>tl.id===action.id ? {...tl, title: action.title} : tl))
-
+            return (state.map(tl => tl.id === action.id ? {...tl, title: action.title} : tl))
+        case 'CHANGE-TODOLIST-FILTER':
+            return (state.map(tl => tl.id === action.id ? {...tl, filter: action.filter} : tl))
         default:
             return state
     }
